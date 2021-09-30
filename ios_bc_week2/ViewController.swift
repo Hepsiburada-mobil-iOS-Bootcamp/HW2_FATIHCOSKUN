b61a3b17f52aec492e89aae89182b601f87f620f
fatihcoskunn <coskun.fatih.94@gmail.com>
2021-09-30 23:25:06 +0300

    Initial Commit

---
+++ ios_bc_week2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_bc_week2
+//
+//  Created by Mac on 30.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
